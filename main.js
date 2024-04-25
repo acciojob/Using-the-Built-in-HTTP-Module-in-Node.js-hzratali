a532be35d01ede6f713f93603efd0e074fc1e4e5
Hazrat Ali <hazrat8143@gmail.com>
2024-04-25 22:28:59 +0530

    Done

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const http = require('http');
+const http = require("http");
+const port = 3000;
 
 // TODO: Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!\n");
+});
 
 // TODO: Listen on port 3000
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 // DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
-
